9731866b5b6b79eda14e5b7af262a58ff7fe0b89
Ludovic THUARD <thuard.ludovic@gmail.com>
2023-02-11 23:06:19 +0100

    Appointements controllers

--- controllers/appointements.controller.js
+++ controllers/appointements.controller.js
@@ -31,3 +31,77 @@ exports.delete = (req, res, next) => {
         .catch(err => { res.send('error: ' + err)
         })
 };
+
+exports.update = (req, res, next) => {
+    Appointement.findOne({
+        where: { id: req.params.id }
+    })
+        .then(appointement => {
+            if(appointement.userId === req.token.userId ) {
+                appointement.update({
+                    date: req.body.date,
+                    time: req.body.time
+                });
+                res.json({ status: appointement.date + ' ' + appointement.time + ' Updated!' })
+            }
+            else {
+                res.json({ status: 'You are not authorized to update this appointement!' })
+            }
+        })
+        .catch(err => { res.send('error: ' + err)
+        })
+}
+
+exports.getOne = (req, res, next) => {
+    Appointement.findOne({
+        where : { id: req.params.id },
+        include: [
+            { model: Professional },
+            { model: User }
+        ]
+    })
+    .then(appointement => {
+        if(!appointement) {
+            res.send('Appointement does not exist')
+        }
+        res.json(appointement)
+    })
+    .catch(err => { res.send('error: ' + err)
+    })
+};
+
+exports.getAllByProfessional = (req, res, next) => {
+    Appointement.findAll({
+        where : { professionalId: req.params.id },
+        include: [
+            { model: Professional },
+            { model: User }
+        ]
+    })
+    .then(appointements => {
+        if(!appointements) {
+            res.send('No appointements for this professional')
+        }
+        res.json(appointements)
+    })
+    .catch(err => { res.send('error: ' + err)
+    })
+};
+
+exports.getAllByUser = (req, res, next) => {
+    Appointement.findAll({
+        where : { userId: req.params.id },
+        include: [
+            { model: Professional },
+            { model: User }
+        ]
+    })
+    .then(appointements => {
+        if(!appointements) {
+            res.send('No appointements for this user')
+        }
+        res.json(appointements)
+    })
+    .catch(err => { res.send('error: ' + err)
+    })
+};
